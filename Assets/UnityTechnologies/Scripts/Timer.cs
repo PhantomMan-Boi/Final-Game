ee9a7356eed92682fa340a54e14310501638419e
Luis Fajardo <lafajardo28@gmail.com>
2024-12-04 13:53:07 -0500

    Initial commit

---
+++ Assets/UnityTechnologies/Scripts/Timer.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float startTime;  
+    private float timeRemaining;   
+    public TextMeshProUGUI timerText;  
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource caughtAudio;
+
+    private float m_Timer = 0f;  
+    public float fadeDuration = 1f;  
+    public float displayImageDuration = 1f;
+    bool m_HasAudioPlayed;
+
+    void Start()
+    {
+        
+        timeRemaining = startTime;
+    }
+
+    void Update()
+    {
+        
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;  
+        }
+        else
+        {
+            timeRemaining = 0;  
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);  
+        }
+
+        int minutes = Mathf.FloorToInt(timeRemaining / 60);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
+        m_Timer += Time.deltaTime;  
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;  
+
+      
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);  
+            }
+            else
+            {
+                Application.Quit();  
+            }
+        }
+    }
+}
